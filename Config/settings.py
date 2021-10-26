fb1607276d5f4af587a094b9641ce249ffae8d4f
koalaalk <site3@koalaeletroshop.com.br>
2021-10-25 21:59:58 -0300

    Update settings.py

--- Config/settings.py
+++ Config/settings.py
@@ -160,6 +160,9 @@ AUTH_USER_MODEL = 'users.User'
 
 #Django allauth
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
