683bb8d1d7002a283cede3252e6f291379c61d0f
koalaalk <site3@koalaeletroshop.com.br>
2021-10-27 19:07:17 -0300

    Update settings.py

--- Config/settings.py
+++ Config/settings.py
@@ -147,6 +147,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_URL = '/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -159,11 +161,13 @@ AUTH_USER_MODEL = 'users.User'
 #Django allauth
 
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 
 SITE_ID = 1
 
